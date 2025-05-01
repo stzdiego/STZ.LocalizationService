262e86b76f9b0cc29ebab98af85728558ac17f6e
Diego Armando Santacruz <diego.santacruz@siesa.com>
2025-05-01 16:40:41 -0500

    Swagger

--- STZ.LocalizationService.Backend/Program.cs
+++ STZ.LocalizationService.Backend/Program.cs
@@ -19,11 +19,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
